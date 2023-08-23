5e2bacecbe9d96fb5465782ab985829e29d2e943
Raam Sai Bharadwaj <143011465+rsb01@users.noreply.github.com>
2023-08-23 21:41:12 +0530

    Create directories.py

---
+++ directories.py
@@ -0,0 +1,26 @@
+train_data_dir/
+    gesture_class_1/
+        image1.jpg
+        image2.jpg
+        ...
+    gesture_class_2/
+        image1.jpg
+        image2.jpg
+        ...
+    ...
+    
+test_data_dir/
+    gesture_class_1/
+        image1.jpg
+        image2.jpg
+        ...
+    gesture_class_2/
+        image1.jpg
+        image2.jpg
+        ...
+    ...
+
+
+//Please note that this is a basic example, and there are many ways to improve the model's performance, 
+such as adjusting hyperparameters, using different architectures, and optimizing data preprocessing. 
+Additionally, you'll need to replace 'train_data_dir' and 'test_data_dir' with the actual paths to your training and testing data directories.
